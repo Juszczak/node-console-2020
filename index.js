6e001a6acdaa465cf239db4f23e4603632f7b2d7
Adrian Juszczak <adrian@juszczak.io>
2020-02-23 16:49:49 +0100

    persist the data inside the db.json file
    Signed-off-by: Adrian Juszczak <adrian@juszczak.io>

--- index.js
+++ index.js
@@ -1,19 +1,34 @@
 const express = require('express');
 const proxy = require('express-http-proxy');
 const bodyParser = require('body-parser');
+const fs = require('fs');
 
 const PORT = +process.env.APP_PORT || 3000;
 const PROXY_PORT = +process.env.PROXY_PORT || 8080;
-const PROXY_URL = 'http://localhost:' + PROXY_PORT
+const PROXY_URL = 'http://localhost:' + PROXY_PORT;
 const NOTES_URL = '/notes';
 const DIST_PATH = './dist';
+const DB_PATH = './db.json';
 const isProduction = process.env.WEBPACK_PRODUCTION;
 
 const app = express();
 
 app.use(bodyParser.json());
 
-const notes = [];
+let notes;
+
+try {
+  const db = fs.readFileSync(DB_PATH);
+  const data = JSON.parse(db);
+  notes = data.notes;
+} catch (error) {
+  notes = [];
+}
+
+function saveDate() {
+  const data = JSON.stringify({notes: notes});
+  fs.writeFileSync(DB_PATH, data);
+}
 
 app.get(NOTES_URL, (req, res) => {
   res.json({
@@ -24,6 +39,7 @@ app.get(NOTES_URL, (req, res) => {
 app.post(NOTES_URL, (req, res) => {
   const note = req.body.note;
   notes.push(note);
+  saveDate();
   res.json({
     created: true,
   });
@@ -33,6 +49,7 @@ app.delete(NOTES_URL, (req, res) => {
   const note = req.body.note;
   const index = notes.indexOf(note);
   notes.splice(index, 1);
+  saveDate();
 
   res.json({
     deleted: true,
