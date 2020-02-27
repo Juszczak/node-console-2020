1e929e746a83232db5e1f7bf734df07d94e2c0ce
Adrian Juszczak <adrian@juszczak.io>
2020-02-27 21:20:26 +0100

    Comments
    Signed-off-by: Adrian Juszczak <adrian@juszczak.io>

--- index.js
+++ index.js
@@ -1,46 +1,57 @@
+/* import zewnętrznych bibliotek */
 const express = require('express');
 const proxy = require('express-http-proxy');
 const bodyParser = require('body-parser');
 
-const PORT = +process.env.APP_PORT || 3000;
-const PROXY_PORT = +process.env.PROXY_PORT || 8080;
-const PROXY_URL = 'http://localhost:' + PROXY_PORT
-const NOTES_URL = '/notes';
-const DIST_PATH = './dist';
-const isProduction = process.env.WEBPACK_PRODUCTION;
+/* deklaracja zmiennych pomocniczych dla aplikacji */
+const PORT = +process.env.APP_PORT || 3000; // numer portu dla aplikacji
+const PROXY_PORT = +process.env.PROXY_PORT || 8080; // numer portu dla proxy webpacka
+const PROXY_URL = 'http://localhost:' + PROXY_PORT // pełny adres URL proxy webpacka
+const NOTES_URL = '/notes'; // adres ścieżki dla endpointów notatek
+const DIST_PATH = './dist'; // ścieżka do zbudowanej aplikacji w systemie plików
+const isProduction = process.env.WEBPACK_PRODUCTION; // zmienna informująca o trybie produkcyjnym
 
+/* utworzenie obiektu reprezentującego server */
 const app = express();
 
+/* wykorzystanie middleware do parsowania ciał zapytań w formacie JSON */
 app.use(bodyParser.json());
 
+/* deklaracja pustej tablicy, w której będą przechowywane dane na serwerze */
 const notes = [];
 
+/* obsługa zapytania typu GET */
 app.get(NOTES_URL, (req, res) => {
+  /* endpoint zwraca JSON z listą notatek */
   res.json({
     notes: notes,
   });
 });
 
+/* obsługa zapytania typu POST – utworzenie nowego zasobu */
 app.post(NOTES_URL, (req, res) => {
-  const note = req.body.note;
-  notes.push(note);
+  const note = req.body.note; // dane przesłane w ciele zapytania
+  notes.push(note); // zapis danych na serwerze
   res.json({
-    created: true,
+    created: true, // zwrócenie informacji o utworzeniu zasobu
   });
 });
 
+/* obsługa zapytania typu DELETE – kasowanie zasobu */
 app.delete(NOTES_URL, (req, res) => {
-  const note = req.body.note;
-  const index = notes.indexOf(note);
-  notes.splice(index, 1);
+  const note = req.body.note; // dane przesłane w ciele zapytania
+  const index = notes.indexOf(note); // indeks tablicy, pod którym znajduje się zasób
+  notes.splice(index, 1); // usunięcie elementu pod wskazanym indeksem
 
   res.json({
-    deleted: true,
+    deleted: true, // zwrócenie informacji o skasowaniu zasobu
   });
 });
 
+/* obsługa serwowania statycznych plików dla zapytań typu GET */
 app.get('*', isProduction ? express.static(DIST_PATH) : proxy(PROXY_URL));
 
+/* rozpoczęcie nasłuchiwania na wskazanym porcie */
 app.listen(PORT, () => {
   console.log('listening on port', PORT);
 });
--- src/main.js
+++ src/main.js
@@ -1,51 +1,64 @@
+/* zmienna pomocnicza zawierająca ścieżkę dla zapytań do API */
 const NOTES_URL = '/notes';
 
+/* obsługa wydarzenia załadowania strony */
 document.addEventListener('DOMContentLoaded', () => {
+  /* deklaracja zmiennych z referencjami do elementów DOM z pliku index.html */
   const content = document.getElementById('content'); // ul
   const todos = document.getElementById('todos'); // form
   const todo = document.getElementById('todo'); // input
+
+  /* deklaracja nagłówków dla zapytań */
   const requestHeaders = {
     'Content-Type': 'application/json',
     Accept: 'application/json',
   };
 
+  /* obsługa wydarzenia próby wysłania formularza */
   todos.addEventListener('submit', event => {
-    event.preventDefault();
-    const note = todo.value;
-    todo.value = '';
+    event.preventDefault(); // zablokowanie domyślnego zachowania przeglądarki
+    const note = todo.value; // przypisanie wartości wpisanej w polu tekstowym
+    todo.value = ''; // wyczyszczenie pola tekstowego
 
-    const body = {note: note};
+    const body = {note: note}; // ciało zapytania POST /notes
 
-    addNote(note);
+    addNote(note); // wywołanie funkcji tworzącej element w drzewie DOM
 
+    /* wysłanie zapytania typu POST */
     fetch(NOTES_URL, {
-      method: 'POST',
-      headers: requestHeaders,
-      body: JSON.stringify(body),
+      method: 'POST', // typ zapytania
+      headers: requestHeaders, // deklaracja nagłówków
+      body: JSON.stringify(body), // serializacja ciała zapytania
     }).then(res => {});
   });
 
+  /* funkcja dodająca element w drzewie DOM */
   function addNote(note) {
-    const element = document.createElement('li');
-    element.innerText = note;
-    content.appendChild(element);
+    const element = document.createElement('li'); // utworzenie elementu <li>
+    element.innerText = note; // deklaracja treści utworzonego elementu
+    content.appendChild(element); // dodanie elementu do istniejącego elementu <ul>
 
+    /* obsługa wydarzenia kliknięcia na element <li> */
     element.addEventListener('click', event => {
+      /* wysłanie zapytania typu DELETE */
       fetch(NOTES_URL, {
-        method: 'DELETE',
-        headers: requestHeaders,
-        body: JSON.stringify({note: note}),
-      }).then(res => {
-        content.removeChild(element);
+        method: 'DELETE', // typ zapytania
+        headers: requestHeaders, // deklaracja nagłówków
+        body: JSON.stringify({note: note}), // serializacja ciała zapytania
+      }).then(res => { // obsługa pomyślnego zapytania
+        content.removeChild(element); // usunięcie elementu z drzewa DOM
       });
     });
   }
 
+  /* pobranie listy elementów po załadowaniu strony */
   fetch(NOTES_URL)
-    .then(res => res.json())
+    .then(res => res.json()) // mapowanie odpowiedzi serwera na ciało odpowiedzi
     .then(body => {
-      content.innerHTML = '';
-      const notes = body.notes;
+      content.innerHTML = ''; // wyczyszczenie tekstu "Loading…"
+      const notes = body.notes; // zmienna pomocnicza (tablica)
+      /* wywołanie funkcji tworzącej pojedynczy element w drzewie DOM
+       * dla każdego z elementów pobranej tablicy */
       notes.forEach(note => addNote(note));
     });
 });
